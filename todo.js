8f5e901597a2affd3f0706fcfc7821de2fc02052
JSingleton81 <ja1981ms@gmail.com>
2024-02-24 12:02:06 -0600

    todo list update

--- todo.js
+++ todo.js
@@ -13,8 +13,8 @@ let arrayOfTodos = [
 }
 ];
 
-  console.log(arrayOfTodos[0].userId) // => 14
-  console.log(arrayOfTodos[1].userId) // => 20
+  // console.log(arrayOfTodos[0].userId) // => 14
+  // console.log(arrayOfTodos[1].userId) // => 20
 
   const userTwenty = arrayOfTodos [1]
   console.log('userTwentyTitle info:', userTwenty.title)
@@ -23,6 +23,7 @@ const fetchTodos = () => {
   fetch('https://jsonplaceholder.typicode.com/todos')
   .then( (response) => response.json())
   .then( (json) => arrayOfTodos = json)
+  
 }
 
 const logTodos = () => {
@@ -43,8 +44,40 @@ const populateTodos = () => {
     listItem.appendChild(textNode);
 
   
-orderedListElement.appendChild(listItem)
+    orderedListElement.appendChild(listItem)
   }
-
-
 }
+
+
+const filteredTodos = () => {
+  clearTodos()
+  const orderedListElement = document.getElementById("todo-list")
+
+  const number = document.getElementById("userId").valueAsNumber
+
+  const filteredArray = arrayOfTodos.filter((arr) => arr.userId === number)
+
+  for (let index = 0; index < filteredArray.length; index++) {
+    const title = filteredArray[index].title;
+    
+    const listItem = document.createElement("li");
+
+    const textNode =document.createTextNode(title);
+
+    listItem.appendChild(textNode);
+
+  
+    orderedListElement.appendChild(listItem)
+  }
+}
+const clearTodos = () => {
+  const orderedListElement = document.getElementsByTagName("ol")
+
+  for (let index = 0; index < orderedListElement.length; index++) {orderedListElement[index].innerHTML = null
+ }
+}
+
+  const filtered = arrayOfTodos.filter((todo) => {
+  console.log('todo:', todo)
+  return todo.userId === 5
+   } )
